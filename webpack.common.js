9dc4099912e9c325524a8f50fe90b64446bebee8
Anna Borja <1243488+annaborja@users.noreply.github.com>
2021-02-20 19:41:59 -0800

    Add hash to bundle output files.

--- webpack.common.js
+++ webpack.common.js
@@ -36,7 +36,7 @@ module.exports = {
     ],
   },
   output: {
-    filename: "[name].bundle.js",
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "docs"),
   },
   plugins: [
