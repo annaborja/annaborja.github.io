b95de4fe5931a2fab73dd1c8c5dec8fc9e9926b2
Anna Borja <1243488+annaborja@users.noreply.github.com>
2023-06-12 23:19:37 -0700

    chore: upgrade to Astro v2 and add MDX integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
+import mdx from "@astrojs/mdx";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [mdx()],
   site: "https://annaborja.com",
 });
